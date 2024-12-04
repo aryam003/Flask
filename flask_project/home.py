51a5a4ffb9afa8e1a44f62eae247f26510e8565f
aryam003 <aryamarya537@gmail.com>
2024-12-04 13:36:15 +0530

    flask

--- flask_project/home.py
+++ flask_project/home.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask,render_template,request
+import sqlite3
 
 app=Flask(__name__)
 
@@ -6,5 +7,20 @@ app=Flask(__name__)
 @app.route('/')
 def fun():
     return "welcome"
+
+@app.route('/fun1',methods=['POST','GET'])
+def fun1():
+    if request.method=='POST':
+        name =request.form['name']
+        place =request.form['place']
+        print(name,place)
+        con=sqlite3.connect('flaskform.db')
+        # con.execute("create table form(name text,place text)")
+        # print('table exists.')    
+        con.execute("insert into form(name,place)values(?,?)",(name,place))
+        con.commit()
+
+    a=20
+    return render_template('index.html',data=a)
 
 app.run()
